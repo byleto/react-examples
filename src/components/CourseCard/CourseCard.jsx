6ec35b59f0f73ce01f1e811f060f6dcab4befacd
Byron Torres <byleto@gmail.com>
2021-08-21 20:46:04 +0200

    BT/JC/LF - Add card for course component

---
+++ src/components/CourseCard/CourseCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  span: {
+    height: 140,
+  },
+  textAlignCourse: {
+    textAlign: "left",
+  },
+});
+
+export default function CourseCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://prod-discovery.edx-cdn.org/media/course/image/dc921dbd-434a-4994-acde-1430679bca2e-5198daeb8d45.small.jpg"
+          title="course image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography
+            className={classes.span}
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          >
+            {props.institutionName}
+          </Typography>
+          <Typography className={classes.textAlignCourse} variant="caption" display="block" gutterBottom>
+            course
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}

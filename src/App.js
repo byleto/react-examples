7b66647ec085a782ca1615c8f613d953b7e906f8
Byron Torres <byleto@gmail.com>
2021-08-21 21:03:19 +0200

    BT/JC/LF - Add grid in app js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
 import CourseCard from "./components/CourseCard/CourseCard";
+import Grid from "@material-ui/core/Grid";
 
 function App() {
   return (
     <div className="App">
-      <CourseCard
-        title="Programming for the Web with JavaScript"
-        institutionName="PennX"
-        coverImage="https://prod-discovery.edx-cdn.org/media/course/image/dc921dbd-434a-4994-acde-1430679bca2e-5198daeb8d45.small.jpg"
-      />
-      <CourseCard
-        title="Computer Science 101"
-        institutionName="StanfordOnline"
-        coverImage="https://prod-discovery.edx-cdn.org/media/course/image/05f61d04-5acc-452b-9c34-6d447af6437f-7a7861d50d40.small.png"
-
-      />
+      <Grid container spacing={1}>
+        <Grid item xs={2}>
+          <CourseCard
+            title="Programming for the Web with JavaScript"
+            institutionName="PennX"
+            coverImage="https://prod-discovery.edx-cdn.org/media/course/image/dc921dbd-434a-4994-acde-1430679bca2e-5198daeb8d45.small.jpg"
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <CourseCard
+            title="Computer Science 101"
+            institutionName="StanfordOnline"
+            coverImage="https://prod-discovery.edx-cdn.org/media/course/image/05f61d04-5acc-452b-9c34-6d447af6437f-7a7861d50d40.small.png"
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }

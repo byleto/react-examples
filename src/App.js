9e687cec4ae899ed06924c87154cda3728c077b6
Byron Torres <byleto@gmail.com>
2021-08-22 02:09:39 +0200

    BT -  Add card component with css only

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import CourseCard from "./components/CourseCard/CourseCard";
+import Card from "./components/Card/Card";
 import Grid from "@material-ui/core/Grid";
 
 function App() {
@@ -18,6 +19,30 @@ function App() {
             title="Computer Science 101"
             institutionName="StanfordOnline"
             coverImage="https://prod-discovery.edx-cdn.org/media/course/image/05f61d04-5acc-452b-9c34-6d447af6437f-7a7861d50d40.small.png"
+          />
+        </Grid>{" "}
+        <Grid item xs={2}>
+          <Card
+            name={"Programming for the Web with JavaScript"}
+            image={
+              "https://prod-discovery.edx-cdn.org/media/course/image/dc921dbd-434a-4994-acde-1430679bca2e-5198daeb8d45.small.jpg"
+            }
+            partnerLogo={
+              "https://prod-discovery.edx-cdn.org/organization/logos/fc03b413-d5d6-48af-974f-21da4d244ee8-6fe2970cf98b.png"
+            }
+            partnerName={"PennX"}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Card
+            name={"Computer Science 101"}
+            image={
+              "https://prod-discovery.edx-cdn.org/media/course/image/05f61d04-5acc-452b-9c34-6d447af6437f-7a7861d50d40.small.png"
+            }
+            partnerLogo={
+              "https://prod-discovery.edx-cdn.org/organization/logos/f53a7458-c79b-4524-97cf-28241114230e-e47d44123c3b.png"
+            }
+            partnerName={"StandforOnline"}
           />
         </Grid>
       </Grid>
---
+++ src/components/Card/Card.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card(props) {
+  return (
+    <div
+      className={
+        "discovery-card col mb-4 scrollable-discovery-card-spacing"
+      }
+      role={"group"}
+      aria-label={props.name}
+    >
+      <a
+        className={"discovery-card-link bg-white text-black"}
+        href={"/course/programming-for-the-web-with-javascript"}
+      >
+        <div className={"d-flex flex-column d-card-wrapper"}>
+          <div className={"d-card-hero"}>
+            <img
+              src={
+                props.image
+              }
+              alt={""}
+              className={"d-card-hero-image"}
+            />
+          </div>
+          <div className={"partner-logo-wrapper"}>
+            <img
+              src={
+                props.partnerLogo
+              }
+              className={"partner-logo"}
+              alt={"University of Pennsylvania"}
+            />
+          </div>
+          <div className={"pl-4 pt-4 mt-2"}>
+            <h3 className={"h4 text-black"}>
+              <span width={"230"}></span>
+              <span>{props.name}</span>
+            </h3>
+            <div className={"text-gray-700 small provider"}>
+              <span width={"220"}>
+                  <span>{props.partnerName}</span>
+              </span>
+            </div>
+          </div>
+          <div
+            className={
+              "d-card-footer x-small ml-4 x-small d-flex flex-column mb-4"
+            }
+          >
+            <div class="d-flex flex-column">
+              <span class="program-badge d-flex justify-content-center align-items-center text-primary-500 mb-2 badge badge-light">
+                <span class="pgn__icon badge-icon">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    focusable="false"
+                    aria-label=""
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M14.263 3H4v11.842h1.466V4.48h8.797V3z"
+                      fill="currentColor"
+                    ></path>
+                    <path
+                      d="M19 6.158H7.158v15H19v-15zm-10.362 1.5h3.7v6l-1.85-1.125-1.85 1.125v-6z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </span>
+                <span>
+                  MicroBachelors<sup classname="superscript">®</sup> Program
+                </span>
+              </span>
+              <span> 4 Courses</span>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+}
